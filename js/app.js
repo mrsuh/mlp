03024b142808d2c5386ef311a473dbca9ec02992
anton <mrsuh6@gmail.com>
2017-03-17 23:13:27 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,221 @@
+var personages = [
+    {
+        id: 'twilight_sparkle',
+        name: 'Twilight Sparkle',
+        img: '/img/rarity.png'
+    },
+    {
+        id: 'spike',
+        name: 'Spike',
+        img: '/img/rarity.png'
+    },
+    {
+        id: 'rainbow_dash',
+        name: 'RainbowDash',
+        img: '/img/personages/rarity.png'
+    },
+    {
+        id: 'rarity',
+        name: 'RainbowDash',
+        img: '/img/rarity.png'
+    },
+    {
+        id: 'fluttershy',
+        name: 'Fluttershy',
+        img: '/img/rarity.png'
+    },
+    {
+        id: 'pinkie_pie',
+        name: 'Pinkamina Diane "Pinkie" Pie',
+        img: '/img/rarity.png'
+    },
+    {
+        id: 'applejack',
+        name: 'Applejack',
+        img: '/img/rarity.png'
+    }
+];
+
+var questions = [
+    {
+        question: 'If you had a power of one of the elements of harmony, which one would you have?',
+        answers: [
+            {
+                answer: 'Magic',
+                personages: ['rainbow_dash'],
+                background: '#8c0188'
+            },
+            {
+                answer: 'Honesty',
+                personages: ['rainbow_dash'],
+                background: '#24b2ca'
+            },
+            {
+                answer: 'Generosity',
+                personages: ['rainbow_dash'],
+                background: '#8c0188'
+            },
+            {
+                answer: 'Fire',
+                personages: ['rainbow_dash'],
+                background: '#24b2ca'
+            },
+            {
+                answer: 'Kindness',
+                personages: ['rainbow_dash'],
+                background: '#8c0188'
+            },
+            {
+                answer: 'Loyalty',
+                personages: ['rainbow_dash'],
+                background: '#8c0188'
+            }
+
+        ]
+    },
+    {
+        question: 'What describes you best?',
+        answers: [
+            {
+                answer: 'Smart',
+                personages: ['rainbow_dash'],
+                background: '#8c0188'
+            },
+            {
+                answer: 'Quiet',
+                personages: ['rainbow_dash'],
+                background: '#8c0188'
+            },
+            {
+                answer: 'Confident',
+                personages: ['rainbow_dash'],
+                background: '#24b2ca'
+            },
+            {
+                answer: 'Teaching',
+                personages: ['rainbow_dash'],
+                background: '#8c0188'
+            },
+            {
+                answer: 'Girly',
+                personages: ['rainbow_dash'],
+                background: '#8c0188'
+            },
+            {
+                answer: 'Silly',
+                personages: ['rainbow_dash'],
+                background: '#8c0188'
+            }
+        ]
+    },
+    {
+        question: 'What is your favorite food?',
+        answers: [
+            {
+                answer: 'Beer',
+                personages: ['rainbow_dash'],
+                background: '#8c0188'
+            },
+            {
+                answer: 'Apples',
+                personages: ['rainbow_dash'],
+                background: '#8c0188'
+            },
+            {
+                answer: 'Rainbow chocolate chips',
+                personages: ['rainbow_dash'],
+                background: '#24b2ca'
+            },
+            {
+                answer: 'Carrots',
+                personages: ['rainbow_dash'],
+                background: '#8c0188'
+            },
+            {
+                answer: 'Salad',
+                personages: ['rainbow_dash'],
+                background: '#8c0188'
+            },
+            {
+                answer: 'Something that will make me strong',
+                personages: ['rainbow_dash'],
+                background: '#8c0188'
+            }
+        ]
+    },
+    {
+        question: 'What do you want to be when you grow up?',
+        answers: [
+            {
+                answer: 'A big, mighty dragon',
+                personages: ['rainbow_dash'],
+                background: '#8c0188'
+            },
+            {
+                answer: 'A comedian',
+                personages: ['rainbow_dash'],
+                background: '#8c0188'
+            },
+            {
+                answer: 'A teacher',
+                personages: ['rainbow_dash'],
+                background: '#24b2ca'
+            },
+            {
+                answer: 'A princess',
+                personages: ['rainbow_dash'],
+                background: '#8c0188'
+            },
+            {
+                answer: 'A farmer',
+                personages: ['rainbow_dash'],
+                background: '#8c0188'
+            },
+            {
+                answer: 'A model',
+                personages: ['rainbow_dash'],
+                background: '#8c0188'
+            }
+        ]
+    },
+    {
+        question: 'What is your casual outfit?',
+        answers: [
+            {
+                answer: 'A mustache',
+                personages: ['rainbow_dash'],
+                background: '#8c0188'
+            },
+            {
+                answer: 'Long, flowing hair',
+                personages: ['rainbow_dash'],
+                background: '#8c0188'
+            },
+            {
+                answer: 'A cowboy/cowgirl hat',
+                personages: ['rainbow_dash'],
+                background: '#24b2ca'
+            },
+            {
+                answer: 'A slim outfit to help me fly faster',
+                personages: ['rainbow_dash'],
+                background: '#8c0188'
+            },
+            {
+                answer: 'A tight dress rarity slipped on me',
+                personages: ['rainbow_dash'],
+                background: '#8c0188'
+            },
+            {
+                answer: 'A beautiful, long and silky gown',
+                personages: ['rainbow_dash'],
+                background: '#8c0188'
+            }
+        ]
+    }
+];
+
+document.addEventListener("DOMContentLoaded", function(){
+    var block = document.getElementById('block');
+    new Test(block, questions, personages);
+});
---
+++ js/test.js
@@ -0,0 +1,191 @@
+var Test = function (block, questions, personages) {
+    this.block = block;
+    this.questions = questions;
+    this.question_index = 0;
+    this.answers = [];
+    this.personages = personages;
+    this.time = null;
+
+    this.init()
+};
+
+Test.prototype.onAnswerClick = function () {
+    var personages = JSON.parse(event.target.getAttribute('data-personages'));
+
+    for (var i = 0, length = personages.length; i < length; i++) {
+        this.answers.push(personages[i]);
+    }
+
+    console.info(this.personages);
+
+    if (++this.question_index >= this.questions.length) {
+        this.showPersonage();
+
+        return false;
+    }
+
+    this.askQuestion();
+};
+
+Test.prototype.init = function () {
+
+    this.answers = [];
+
+    var play = document.createElement('DIV');
+
+    play.className = 'play';
+    play.innerText = 'start';
+
+    this.block.appendChild(play);
+
+    play.addEventListener('click', function () {
+        this.askQuestion();
+    }.bind(this));
+};
+
+Test.prototype.showPersonage = function () {
+
+    var div = this.block.querySelector('.wrapper');
+
+    div.style.opacity = 0;
+
+    div.addEventListener("transitionend", function () {
+
+        while (this.block.firstChild) {
+            this.block.removeChild(this.block.firstChild);
+        }
+
+        var personage = this.mathPersonageByAnswers();
+
+        var block = document.createElement('DIV');
+        block.style.opacity = 0;
+
+        var text = document.createElement('H2');
+
+        text.innerHTML = 'Congratulations! <br> Your pony is "' + personage.name + '"';
+
+        var image = document.createElement('IMG');
+
+        image.src = personage.img;
+
+        console.info('DONE');
+
+        block.appendChild(text);
+        block.appendChild(image);
+
+        this.block.appendChild(block);
+
+
+        setTimeout(function () {
+            block.style.opacity = 1;
+        }, 1);
+
+    }.bind(this));
+
+};
+
+Test.prototype.mathPersonageByAnswers = function () {
+    var personages = [];
+
+    for (var i = 0, length = this.answers.length; i < length; i++) {
+        var answer = this.answers[i];
+        if ('undefined' === typeof personages[answer]) {
+            personages[answer] = 1;
+        }
+
+        personages[answer]++;
+    }
+
+    console.info(personages);
+
+    var max = 0;
+    var personage = null;
+
+    for (var key in personages) {
+        if (personages[key] > max) {
+            max = personages[key];
+            personage = key;
+        }
+    }
+
+    console.info(personage);
+
+    for (var i = 0, length = this.personages.length; i < length; i++) {
+
+        if (this.personages[i].id === personage) {
+            return this.personages[i];
+        }
+    }
+
+    return null;
+};
+
+Test.prototype.askQuestion = function () {
+
+    this.time = new Date().getTime();
+    if (this.question_index == 0) {
+
+        this.rebderQuestion();
+
+    } else {
+        var div = this.block.querySelector('.wrapper');
+
+        div.style.opacity = 0;
+
+        div.addEventListener("transitionend", function () {
+
+            this.rebderQuestion();
+
+        }.bind(this));
+    }
+};
+
+Test.prototype.rebderQuestion = function () {
+    while (this.block.firstChild) {
+        this.block.removeChild(this.block.firstChild);
+    }
+
+    var block_question = document.createElement('DIV');
+
+    block_question.className = 'wrapper';
+    block_question.style.opacity = 0;
+
+    var div_question = document.createElement('DIV');
+
+    var question = this.questions[this.question_index];
+
+    div_question.innerText = '[' + (this.question_index + 1) + '/' + (this.questions.length) + '] ' + question.question;
+    div_question.className = 'question';
+
+    block_question.appendChild(div_question);
+    var answers = [];
+    for (var i = 0, length = question.answers.length; i < length; i++) {
+
+        var answer = question.answers[i];
+
+        var div_answer = document.createElement('DIV');
+        div_answer.innerText = answer.answer;
+        div_answer.setAttribute('data-personages', JSON.stringify(answer.personages));
+        div_answer.className = 'answer';
+        div_answer.style.background = answer.background;
+        div_answer.addEventListener('click', function (event) {
+
+            console.info('TIME: ' + (new Date().getTime() - this.time));
+
+            if (new Date().getTime() - this.time < 300) {
+                return false;
+            }
+
+            this.onAnswerClick(event);
+        }.bind(this));
+
+        answers.push(div_answer);
+        block_question.appendChild(div_answer);
+    }
+
+    this.block.appendChild(block_question);
+
+    setTimeout(function () {
+        block_question.style.opacity = 1;
+    }, 1);
+};
